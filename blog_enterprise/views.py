b1170b3a3a9dde134b0264ae8e4c62ee00555612
Bladimir Ramos <bladi.ram@gmail.com>
2024-07-18 12:29:43 -0400

    Renderizado y consulta de posts

--- blog_enterprise/views.py
+++ blog_enterprise/views.py
@@ -1,11 +1,13 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-from blogs.models import Category
+from blogs.models import Category, Blog
 
 
 def home(request):
     categories = Category.objects.all()
+    posts = Blog.objects.filter(is_featured=True).order_by('-updated_at')
     context = {
-        'categories': categories
+        'categories': categories,
+        'featured_posts': posts
     }
     return render(request, 'home.html', context)
