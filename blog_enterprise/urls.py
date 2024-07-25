bc2aa2952bfabb656c492bb96fe33acea4ca51ff
Bladimir Ramos <bladi.ram@gmail.com>
2024-07-25 15:52:36 -0400

    Agregado al funcionalidad del template de artículos

--- blog_enterprise/urls.py
+++ blog_enterprise/urls.py
@@ -19,9 +19,11 @@ from django.urls import path, include
 from . import views
 from django.conf.urls.static import static
 from django.conf import settings
+from blogs import views as BlogViews
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
     path('category/', include('blogs.urls')),
+    path('blog/<slug:slug>', BlogViews.blogs, name='blogs'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
